e92f1432c15271e507d58d6e774056498c366454
Abhieeeh <abhishekkvilakkode@gmail.com>
2026-04-02 17:05:45 +0530

    Overview of w.app created

--- wether.js
+++ wether.js
@@ -1,9 +1,12 @@
 import express from "express";
 import axios from "axios"
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
 
 const app= express();
 const port=3000;
+dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -23,28 +26,21 @@ app.get("/",(req,res)=>{
 app.post("/result", async (req,res)=>{
    try{
     const city=req.body.city;
-    console.log("helloiiii"+ city);
-    const apiKey="0e3067c6be49466f7523543a89ea60f3";
-    const response= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const response= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.Api_key}&units=metric`);
     const data=response.data;
-    console.log(data);
     res.render("weatherindex",{
     Cityname:city,
-    tempindegree: data.main.temp ,
-    humidpercent:data.main.humidity,
-    windspeed:data.wind.speed,
-
+    tempindegree: data.main.temp + " °C",
+    humidpercent:data.main.humidity + " %",
+    windspeed:data.wind.speed + " Km/h",
     });
    } catch(error){
     console.log("error");
     res.render("weatherindex",{Cityname:"City Not Found",tempindegree:"",humidpercent:"",windspeed:""});
-   }
-    
-
-    
-    
-   
+   } 
 });
+
+
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`);
 });
